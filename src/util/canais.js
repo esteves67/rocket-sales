58598395d4b49c7cb9b360a71363a0e6c2e03e81
Claudio Scarpa Filho <claudio@amaro.com.br>
2020-08-06 14:12:10 -0300

    Update canais.js

--- src/util/canais.js
+++ src/util/canais.js
@@ -60,14 +60,17 @@ exports.listarMensagens = async (idRocketLead) => {
 
     const resultWp = await pool
       .request()
-      .input('telefone1', sql.VarChar, '55' + result1[0].telefone1.toString().replace(/\D/g, ''))
-      .input('telefone11', sql.VarChar, '55' + result1[0].telefone1.toString().replace(/\D/g, ''))
-      .input('telefone2', sql.VarChar, '55' + result1[0].telefone2.toString().replace(/\D/g, ''))
-      .input('telefone22', sql.VarChar, '55' + result1[0].telefone.toString().replace(/\D/g, ''))
+      .input('telefone1', sql.BigInt, '55' + result1[0].telefone1.replace(/\D/g, ''))
+      .input('telefone11', sql.BigInt, '55' + result1[0].telefone1.replace(/\D/g, ''))
+      .input('telefone2', sql.BigInt, '55' + result1[0].telefone2.replace(/\D/g, ''))
+      .input('telefone22', sql.BigInt, '55' + result1[0].telefone.replace(/\D/g, ''))
       .query(
         `SELECT id, case when tipo = 'in' then remetente else telefone end remetente, mensagem, CONCAT(CONVERT(VARCHAR(20), data, 103), ' ', CONVERT(VARCHAR(20), data, 108)) data, 'whatsapp' tipo, tipo direcao, DATEDIFF(SECOND,{d '1970-01-01'}, data) timestamp FROM WHATSAPP.MENSAGENS where remetente = @telefone1 or telefone = @telefone11 or remetente = @telefone2 or telefone = @telefone22`
       );
     resultEm.push(...resultWp.recordset);
+
+    console.log(resultWp)
+    console.log(resultEm)
 
     const result = resultEm.sort((a, b) => {
       return new Date(a.timestamp) - new Date(b.timestamp);
